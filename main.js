3dee6487ac960be63b14d16ff5e01f828267c3b0
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-06-19 19:40:25 -0500

    started quiz over

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
-// Could not get it to make a tree shape then tried to "fix it" and now it looks nothing like a tree
-var makeTree = {};
+// After I did the a few of the SPA exercises i'm understanding more kind of??
+let treeInput = {
+	height: 0,
+	character: " "
+};
+let characterInput = document.getElementById("character");
+let heightInput = document.getElementById("height");
+let button = document.getElementById("submit");
+let enterKey = document.getElementById("height")
 
-var button = document.getElementById("submit");
-var enterHeight = document.getElementById("charNumber");
-var enterChar = document.getElementById("charType");
-var enterButton = document.getElementById("treeBuilder");
-
-
-function littleTree(clickEvent) {
-	makeTree.height = document.getElementById("charNumber").value;
-	makeTree.char = document.getElementById("charType").value;
-	buildTree(makeTree);
+function makeTree(){
+	for (var i = 0; i < heightInput.value; i++) {
+		treeInput.height = treeInput.character.repeat(heightInput.value - i) + characterInput.value.repeat(i * 2 + 1);
+		console.log(treeInput.height)
+	}
 }
 
-function buildTree(makeTree) {
-	var spaceNum = makeTree.height;
-	var charNum = makeTree.char;
-	if (enterChar.value !== "" && enterHeight.value !== "") {
-		for(i = 0; i <= makeTree.height; i++) {
-			var spaces = makeTree.height.repeat(spaceNum + 2);
-			var chars = makeTree.char.repeat(charNum - 1);
-			var tree = spaces + chars;
-			console.log(tree);
-		}
+button.addEventListener("click", function(){
+	if (characterInput.value == "" || heightInput.value == ""){
+		alert("Both fields must have a value")
 	} else {
-		alert("Both fields must have a value.");
+		return makeTree()
 	}
-}
+})
 
-button.addEventListener("click", littleTree);
 
-enterButton.addEventListener("keypress", function(event) {
-	if (this.key === "Enter") {
-		littleTree();
-	}
-});
+
+
+
+
